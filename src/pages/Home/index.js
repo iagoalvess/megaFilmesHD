1e0d50679b120b84dba117a90c89d729b01d0aa6
iagoalvess <iagoalves.voador@gmail.com>
2024-02-27 10:48:39 -0300

    comecando com a API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import api from '../../services/api'
+
 function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes(){
+      const response = await api.get("movie/now_playing", {
+        params:{
+          api_key: "6e0f2354253cbc81f0230ee6c36460f5",
+          language: "pt-BR",
+          page: 1,
+        }
+      })
+
+      console.log(response.data.results);
+    }
+
+    loadFilmes();
+  }, [])
+
   return(
     <div>
       <h1>TESTE HOME</h1>
