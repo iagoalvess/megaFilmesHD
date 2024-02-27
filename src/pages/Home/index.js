1b94f20cfdafaabc211022657a7ae56e3fe941be
iagoalvess <iagoalves.voador@gmail.com>
2024-02-27 11:07:31 -0300

    colocando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import api from '../../services/api'
+import api from '../../services/api';
+import { Link } from "react-router-dom";
+import './home.css';
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
@@ -14,15 +16,25 @@ function Home() {
         }
       })
 
-      console.log(response.data.results);
+      setFilmes(response.data.results.slice(0, 10))
     }
 
     loadFilmes();
   }, [])
 
   return(
-    <div>
-      <h1>TESTE HOME</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
     </div>
   )
 }
