8d37b003902d214776222192431534d95f92d411
iagoalvess <iagoalves.voador@gmail.com>
2024-02-27 11:22:57 -0300

    pagina erro e loading

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,7 @@ import './home.css';
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes(){
@@ -16,11 +17,20 @@ function Home() {
         }
       })
 
-      setFilmes(response.data.results.slice(0, 10))
+      setFilmes(response.data.results.slice(0, 10));
+      setLoading(false);
     }
 
     loadFilmes();
   }, [])
+
+  if(loading) {
+    return(
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    )
+  }
 
   return(
     <div className="container">
