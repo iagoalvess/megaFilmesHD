0f212c89d206ba5196d382446993393e916ba051
iagoalvess <iagoalves.voador@gmail.com>
2024-02-27 10:15:49 -0300

    adicionando rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import RoutesApp from "./routes";
 
 function App() {
   return (
     <div className="App">
-      <h1>TESTE</h1>
+      <RoutesApp/>
     </div>
-  );
+  ); 
 }
 
 export default App;
---
+++ src/pages/Filme/index.js
@@ -0,0 +1,9 @@
+function Filme() {
+  return(
+    <div>
+      <h1>TESTE FILME</h1>
+    </div>
+  )
+}
+
+export default Filme;
